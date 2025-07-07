0eed8d4cd6bdaf85a2d39027cdcaf8755f00a9de
Graham Strickland <grahamhstrickland@gmail.com>
2025-07-07 13:38:32 +0200

    Add arrows to plot for exercise 2.1.1

--- src/ch02/2_2_1.jl
+++ src/ch02/2_2_1.jl
@@ -7,15 +7,19 @@ stable_points = [Point2f(-4,0)]
 unstable_points = [Point2f(4,0)]
 
 x = range(-5, 5, length=100)
+ẋ(x) = x^2 - 16
 
-function ẋ(x)
-    return x^2 - 16
-end
+arrow_xs = [-5, -3, 3, 5]
+arrow_dxs = ẋ.(arrow_xs)
+arrow_us = [x < -4|| x > 0 && x < 4 ? x + 0.5 : x - 0.5 for x in arrow_xs]
+arrow_vs = ẋ.(arrow_us)
 
 lines!(ax, x, ẋ, label=L"\dot{x} = x^2 - 16", color="blue")
 scatter!(ax, stable_points, marker=:circle, markersize=10, color="blue")
 scatter!(ax, unstable_points, marker=:circle, markersize=10, color="blue")
 scatter!(ax, unstable_points, marker=:circle, markersize=5, color="white")
+arrows2d!(ax, arrow_xs, arrow_dxs, arrow_us, arrow_vs, shaftwidth=1, tipwidth=10, color="blue", argmode=:endpoint)
+
 axislegend(position=:rt)
 
 save("plots/ch02/ex2_2_1.pdf", fig)
