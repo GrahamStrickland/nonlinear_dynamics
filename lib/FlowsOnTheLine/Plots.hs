044ccf814709ecd7867a6087aba17f31c1e2a8cf
Graham Strickland <grahamhstrickland@gmail.com>
2025-07-25 18:20:36 +0200

    Add matplotlib plotting to exercise 2.2.1

--- lib/FlowsOnTheLine/Plots.hs
+++ lib/FlowsOnTheLine/Plots.hs
@@ -2,23 +2,37 @@
 
 module FlowsOnTheLine.Plots where
 
-import Graphics.Gnuplot.Simple
+import Graphics.Matplotlib
 
 type R = Double
 
 xRange :: [R]
-xRange = [-3,-2.9..3]
+xRange = [-3, -2.9 .. 3]
 
-xDotPos :: R -- x
-        -> R -- xDot
-xDotPos x = 4 * x**2 - 16
+xDotPos ::
+    R ->
+    R
+xDotPos x = 4 * x ** 2 - 16
 
-plot2_1 :: IO ()
-plot2_1
-    = plotFunc [Title "Exercise 2.2.1"
-               ,XLabel "x"
-               ,YLabel "dx/dt"
-               ,PNG "plots/ch02/ex2_2_1.png"
-               ,Key Nothing
-               ] xRange xDotPos
+plot2_2_1 :: Matplotlib
+plot2_2_1 =
+    setTeX True
+        % plot x y
+        @@ [o1 "b"]
+        % spine "bottom"
+        % spineSetPosition "'data'" (0 :: R)
+        % spine "left"
+        % spineSetPosition "'data'" (0 :: R)
+        % spine "right"
+        % spineSetVisible False
+        % spine "top"
+        % spineSetVisible False
+        % xlabel "$x$"
+        % ylabel "$\\dot{x}$"
+        % title "Ex. 2.2.1"
+  where
+    x = xRange
+    y = map xDotPos x
 
+output2_2_1 :: IO (Either String String)
+output2_2_1 = (file "plots/ch02/ex2_2_1.pdf") $ plot2_2_1
