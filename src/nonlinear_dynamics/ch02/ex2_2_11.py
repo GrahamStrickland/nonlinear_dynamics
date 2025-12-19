38bb94cbd0de983adc0c5b8a04e633fffe4eead9
Graham Strickland <grahamhstrickland@gmail.com>
2025-12-19 19:25:12 +0200

    Add initial conditions and type hint to exercise 2.2.11

--- src/nonlinear_dynamics/ch02/ex2_2_11.py
+++ src/nonlinear_dynamics/ch02/ex2_2_11.py
@@ -5,6 +5,6 @@ from sympy.abc import t, V, R, C
 def solution_ex2_2_11():
     Q = Function("Q")
 
-    soln = dsolve(Derivative(Q(t), t) + (Q(t) / R * C) - (V / R), Q(t))
+    soln = dsolve(Derivative(Q(t), t) + (Q(t) / (R * C)) - (V / R), Q(t), ics={Q(0): 0})  # type: ignore
     print("Exercise 2.2.11:")
     print(soln)
